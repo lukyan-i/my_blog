8b966d7d24b32bd9ca5c67ac104d682af835d686
lukjanenko <lukyan-i@yandex.ua>
2014-02-12 16:36:37 +0200

    devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,21 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!
   def new
     @post = Post.new
+
   end
 
   def create
-    @post = Post.new(post_params)
+
+    @post = current_user.posts.new(post_params)
+
       if @post.save
         redirect_to @post
       else
         render 'new'
       end
+
+
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 MyBlog::Application.routes.draw do
+  devise_for :users
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'posts#index'
   resources :posts do
     resources :comments
   end
